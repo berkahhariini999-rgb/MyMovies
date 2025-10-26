2f0b40809659d644fbb0c82720f37dd5b32208ed
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-26 19:56:11 +0700

    Initial Commit

---
+++ MyMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMovies
+//
+//  Created by Iqbal Alhadad on 26/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
