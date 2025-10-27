669a3ffddc1952490b6921264ab967b26d95d75c
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-27 08:38:29 +0700

    add constant

---
+++ MyMovies/Features/Base/Utils/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  MyMovies
+//
+//  Created by Iqbal Alhadad on 27/10/25.
+//
+
+import Foundation
+
+enum Constant {
+    static var apiKey = ""
+    static var imageBaseURL = "https://image.tmdb.org/t/p/original"
+    static var apiBaseURL = "https://api.themoviedb.org"
+}
